6ffe8ecc8416dad9a57a6e287e55658587fdb947
Roman Rychkov <roman.rychkov@outlook.com>
2023-11-21 00:02:15 +0300

    Add project files.

---
+++ AreaCalculator/Calculator.cs
@@ -0,0 +1,100 @@
+﻿using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace AreaCalculator
+{
+    public class Calculator
+    {
+        static SortedDictionary<string, Figure> supportedEquestions = new SortedDictionary<string, Figure>();
+
+        #region Task requests
+        static Calculator()
+        {
+            var cube = new Figure("Circle");
+            cube.AddFigureDefinition("PI*[r]*[r]", ("r", "Radius"));
+            supportedEquestions.Add(cube.Name, cube);
+
+            var triangle = new Figure("Triangle");
+            triangle.AddFigureDefinition("sqrt(([a]+[b]+[c])/2 * (([a]+[b]+[c])/2 - [a]) * (([a]+[b]+[c])/2 - [b]) * (([a]+[b]+[c])/2 - [c]))", ("a", "Second side"), ("b", "First side"), ("c", "Third side"));
+            supportedEquestions.Add(triangle.Name, triangle);
+        }
+        #endregion
+
+        public static List<Figure> GetSupportedFigures()
+        {
+            return new List<Figure>(supportedEquestions.Values);
+        }
+
+        public static void AddCustomFigureDefinition(string figureName, string areaEquation, params (string Label, string Description)[] defintionParameters)
+        {
+            if (!supportedEquestions.TryGetValue(figureName, out var existing))
+            {
+                existing = new Figure(figureName);
+                supportedEquestions.Add(figureName, existing);
+            }
+            existing.AddFigureDefinition(areaEquation, defintionParameters);
+        }
+
+        public static float CalculateTriangleArea(float sideA, float sideB, float sideC)
+        {
+            List<float> sides = new List<float>() { sideA, sideB, sideC };
+            sides.Sort();
+            if (Math.Pow(sides[0], 2) + Math.Pow(sides[0], 2) == Math.Pow(sides[2], 2))
+                return sides[0] * sides[1] / 2f;
+            else
+            {
+                float halfPerimeter = (sideA + sideB + sideC) / 2f;
+                return (float)Math.Sqrt(halfPerimeter * (halfPerimeter - sideA) * (halfPerimeter - sideB) * (halfPerimeter - sideC));
+            }
+        }
+    }
+
+    public class FigureDefintionToAreaEquation
+    {
+        public Dictionary<string, string> ParametersDescriptions;
+        public string AreaEquation;
+
+        public FigureDefintionToAreaEquation(string areaEquation, params (string Name, string Description)[] defintionParameters)
+        {
+            AreaEquation = areaEquation;
+            ParametersDescriptions = defintionParameters.ToDictionary(x => x.Name, x => x.Description);
+        }
+
+        public float Calculate(params (string ParameterName, float ParameterValue)[] values)
+        {
+            string parsedEquation = AreaEquation;
+
+            foreach (var i in values.ToList().OrderByDescending(x => x.ParameterName.Length))
+            {
+                parsedEquation = parsedEquation.Replace($"[{i.ParameterName}]", $"({i.ParameterValue})");
+            }
+            return Convert.ToSingle( Z.Expressions.Eval.Execute(parsedEquation));
+        }
+
+        static float Distance(float x1, float y1, float x2, float y2)
+        {
+            float xOffset = Math.Abs(x2 - x1), yOffset = Math.Abs(y2 - y1);
+            return (float)Math.Sqrt(xOffset * xOffset + yOffset * yOffset);
+        }
+    }
+
+    public class Figure
+    {
+        public string Name;
+        public List<FigureDefintionToAreaEquation> FigureDefintionToAreaEquations;
+
+
+        internal Figure(string name)
+        {
+            this.Name = name;
+            FigureDefintionToAreaEquations = new List<FigureDefintionToAreaEquation>();
+        }
+
+        public void AddFigureDefinition(string areaEquation, params (string Name, string Description)[] defintionParameters)
+        {
+            FigureDefintionToAreaEquations.Add(new FigureDefintionToAreaEquation(areaEquation, defintionParameters));
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,97 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AreaCalculator;
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+while (true)
+{
+    try
+    {
+        Console.WriteLine("What to do?\n1 - Calculate area for existing figures\n2 - Add new figure");
+        int mode = Convert.ToInt32(Console.ReadLine());
+        switch (mode)
+        {
+            case 1:
+                Console.WriteLine("Pick a figure");
+                var figures = Calculator.GetSupportedFigures();
+                int index = 1;
+                foreach (var i in figures)
+                {
+                    Console.WriteLine($"{index++} - {i.Name}");
+                }
+                mode = Convert.ToInt32(Console.ReadLine());
+                mode--;
+                if (mode < 0 || mode >= figures.Count)
+                    continue;
+
+                var targetFigure = figures[mode];
+
+                FigureDefintionToAreaEquation targetParameters;
+
+                if (targetFigure.FigureDefintionToAreaEquations.Count > 1)
+                {
+                    Console.WriteLine("Pick a definition parameters");
+                    index = 1;
+                    foreach (var i in targetFigure.FigureDefintionToAreaEquations)
+                    {
+                        Console.WriteLine($"{index++}:");
+                        Console.WriteLine($" Parameters:");
+                        foreach (var j in i.ParametersDescriptions)
+                            Console.WriteLine($"  {j.Key} - {j.Value}");
+                    }
+
+                    mode = Convert.ToInt32(Console.ReadLine());
+                    mode--;
+
+                    if (mode < 0 || mode >= targetFigure.FigureDefintionToAreaEquations.Count)
+                        continue;
+
+                    targetParameters = targetFigure.FigureDefintionToAreaEquations[mode];
+                }
+                else
+                {
+                    targetParameters = targetFigure.FigureDefintionToAreaEquations.First();
+                    Console.WriteLine($" Parameters:");
+                    foreach (var j in targetParameters.ParametersDescriptions)
+                        Console.WriteLine($"  {j.Key} - {j.Value}");
+                 
+                }
+
+               
+                (string Name, float Value)[] values = new (string Name, float Value)[targetParameters.ParametersDescriptions.Count];
+                index = 0;
+                foreach (var i in targetParameters.ParametersDescriptions)
+                {
+                    Console.Write($"{i.Key} = ");
+                    values[index++] = (i.Key, Convert.ToSingle(Console.ReadLine()));
+                }
+
+                Console.WriteLine($"Area is {targetParameters.Calculate(values)}");
+
+                break;
+            case 2:
+                Console.Write("Write figure name: ");
+                string figureName = Console.ReadLine();
+                Console.Write("Write number of parameters: ");
+                int nParameters = Convert.ToInt32(Console.ReadLine());
+                (string Name, string Descriptions)[] parameters = new (string Name, string Descriptions)[nParameters];
+                for(int i = 0; i < nParameters; i++)
+                {
+                    Console.Write("Parameter Name: ");
+                    string name = Console.ReadLine();
+                    Console.Write("Parameter Description (May be skipped): ");
+                    parameters[i] = (name, Console.ReadLine());
+                }
+                Console.Write("Type Area equation (parameters should be quoted into '[]' e.g. sqrt(10+[a]): ");
+                string areaEquation = Console.ReadLine();
+                Calculator.AddCustomFigureDefinition(figureName, areaEquation, parameters);
+                break;
+            default:
+                continue;
+        }
+    }
+    catch
+    {
+
+    }
+}
